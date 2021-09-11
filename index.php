4c7d02430ffb69d8f39b9d494a01f9941c86bfd8
ayushskmaurya <ayushskmaurya@gmail.com>
2021-09-11 13:40:50 +0530

    Included index page

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>HelloWorld Server</title>
+	<link rel="icon" type="image/ico" href="static/images/logo.ico">
+	<link rel='stylesheet' type='text/css' href="static/css/index.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@900&display=swap" rel="stylesheet">
+</head>
+<body>
+	<img src="static/images/logo.jpg" alt="HelloWorld Server">
+	<h1>HelloWorld Server</h1>
+</body>
+</html>
