4b4d114d114dedae3c44b582d9b464e22aff3014
ayushskmaurya <ayushskmaurya@gmail.com>
2021-09-28 15:54:02 +0530

    Retrieving messages

--- retrieve.php
+++ retrieve.php
@@ -7,6 +7,9 @@
 		if (isset($_POST['WHERE']))
 			$sql .= (" WHERE " . $_POST['WHERE']);
 		
+		if (isset($_POST['ORDER_BY']))
+			$sql .= (" ORDER BY " . $_POST['ORDER_BY']);
+
 		$stmt = $conn->prepare($sql);
 		$stmt->execute();
 
