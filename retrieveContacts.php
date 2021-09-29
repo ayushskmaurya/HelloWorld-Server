997b19d0ffab8e6f87c07ab198115fe7e2c55128
ayushskmaurya <ayushskmaurya@gmail.com>
2021-09-30 00:04:47 +0530

    Retrieving contacts

---
+++ retrieveContacts.php
@@ -0,0 +1,25 @@
+<?php
+	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+		include 'connection.php';
+
+		$phone_nos = $_POST['phone_nos'];
+		$user_id = $_POST['userid'];
+
+		$phone_nos = str_replace("]", ")", str_replace("[", "(", $phone_nos));
+
+		$sql = "SELECT users.userid, users.name, chats.chatid ";
+		$sql .= "FROM (SELECT userid, name FROM users WHERE mobile_no IN $phone_nos) AS users ";
+		$sql .= "LEFT OUTER JOIN chats ";
+		$sql .= "ON (users.userid=chats.user1id AND :user_id=chats.user2id) OR (users.userid=chats.user2id AND :user_id=chats.user1id)";
+		
+		$stmt = $conn->prepare($sql);
+		$stmt->execute(array("user_id" => $user_id));
+
+		$res = array();
+		while($row = $stmt->fetch(PDO::FETCH_ASSOC))
+			array_push($res, $row);
+		
+		header('Content-Type: application/json');
+		echo json_encode($res);
+	}
+?>
